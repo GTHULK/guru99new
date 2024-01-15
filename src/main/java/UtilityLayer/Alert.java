522a7116ce1cadd5374998ded296baff736d45e0
admin <totareg@gmail.com>
2024-01-15 07:45:25 -0800

    i added alert pop reusable script code

--- src/main/java/UtilityLayer/Alert.java
+++ src/main/java/UtilityLayer/Alert.java
@@ -1,5 +1,46 @@
 package UtilityLayer;
 
-public class Alert {
+import BaseLayer.BaseClass;
 
+public class Alert extends BaseClass{
+	
+	public static void clickOnOkbtn() {
+	    driver.switchTo().alert().accept();
+	}
+
+	public static void clickOncancelbtn() {
+		driver.switchTo().alert().dismiss();
+	}
+	
+	public static void sendkeys(String value) {
+		driver.switchTo().alert().sendKeys(value);
+	}
+	
+	public static String captureText() {
+		return driver.switchTo().alert().getText();
+	}
+	
+	public static void EnterTextandClickonOk(String value) {
+		driver.switchTo().alert().sendKeys(value);
+		driver.switchTo().alert().accept();
+	}
+	
+	public static void EnterTextandClickoncancel(String value) {
+		driver.switchTo().alert().sendKeys(value);
+		driver.switchTo().alert().dismiss();
+	}
+	
+	public static String captureTextandClickonOk() {
+		String value=driver.switchTo().alert().getText();
+		driver.switchTo().alert().accept();
+		return value;
+	}
+	
+	public static String captureTextandClickonCancel() {
+		String value=driver.switchTo().alert().getText();
+		driver.switchTo().alert().dismiss();
+		return value;
+	}
+	
 }
+
