9c9a309768c190a4776badd12b612b0b841e7404
Harshada <baravkar.harshada@gmail.com>
2024-01-16 14:56:53 +0530

    Harshada Utility Layer Try

--- src/main/java/UtilityLayer/Wait.java
+++ src/main/java/UtilityLayer/Wait.java
@@ -1,5 +1,20 @@
 package UtilityLayer;
 
-public class Wait {
+import java.time.Duration;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import BaseLayer.BaseClass;
+
+public class Wait extends BaseClass{
+
+	public void click(WebElement wb)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+		WebElement WebWb = wait.until(ExpectedConditions.elementToBeClickable(wb));
+		WebWb.click();
+	}
+	
 }
