7f88845e0b9738b396b5ac145984cffbf4aa00f0
Harshada <baravkar.harshada@gmail.com>
2024-01-17 12:31:46 +0530

    Wait Class updated

--- src/main/java/UtilityLayer/Wait.java
+++ src/main/java/UtilityLayer/Wait.java
@@ -17,4 +17,19 @@ public class Wait extends BaseClass{
 		WebWb.click();
 	}
 	
+
+	public void clear(WebElement wb)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+		WebElement WebWb = wait.until(ExpectedConditions.elementToBeClickable(wb));
+		WebWb.clear();;
+	}
+	
+	public static String getAttribute(WebElement wb, String keyName)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+		WebElement WebWb = wait.until(ExpectedConditions.visibilityOf(wb));
+		String a=WebWb.getAttribute(keyName);
+		return a;
+	}
 }
